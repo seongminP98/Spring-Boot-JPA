9a71046225e9bf125bb59d5ede90fc51be533d9c
seongmin <psm7335@gmail.com>
2021-08-25 00:52:47 +0900

    영속성 컨텍스트

--- JPA-BASIC/src/main/java/hellojpa/JpaMain.java
+++ JPA-BASIC/src/main/java/hellojpa/JpaMain.java
@@ -29,6 +29,7 @@ public class JpaMain {
             findMember.setName("HelloJPA");
 */
 
+/*
             List<Member> result = em.createQuery("select m from Member as m", Member.class)
                     .setFirstResult(5)
                     .setMaxResults(8)
@@ -37,6 +38,41 @@ public class JpaMain {
             for (Member member : result) {
                 System.out.println("member.name = " + member.getName());
             }
+*/
+/*
+            //비영속
+            Member member = new Member();
+            member.setId(101L);
+            member.setName("HelloJPA");
+            //영속
+            em.persist(member);
+
+            Member findMember = em.find(Member.class, 101L);
+            System.out.println("findMember.id = " + findMember.getId());
+            System.out.println("findMember.name = " + findMember.getName()); //1차 캐시에서 가져옴.
+*/
+
+/*
+            Member findMember1 = em.find(Member.class, 101L); //DB에서 가져와 영속성 컨텍스트에 올려놓음
+            Member findMember2 = em.find(Member.class, 101L); //1차 캐시에서 가져옴.
+
+            System.out.println("result = " + (findMember1 == findMember2)); //영속 엔티티의 동일성 보장
+*/
+
+/*쓰기 지연. 커밋하고 DB에 쿼리 날림.
+            Member member1 = new Member(150L, "A");
+            Member member2 = new Member(160L, "B");
+
+            em.persist(member1);
+            em.persist(member2);
+            System.out.println("======================");
+*/
+
+            //데이터를 가져오고 값만 바꿔오면 수정됨. 변경감지(Dirty checking). 트랜잭션 커밋하는 시점에 업데이트 쿼리 날림.
+            Member member = em.find(Member.class, 150L);
+            member.setName("ZZZZZ"); //
+
+
 
             tx.commit();
         } catch (Exception e) {
