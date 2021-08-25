4513373cd33cba4812836fa1817520f820e28090
seongmin <psm7335@gmail.com>
2021-08-26 01:01:57 +0900

    양방향 연관관계와 연관관계의 주인 1 - 기본

--- JPA-BASIC/src/main/java/hellojpa/JpaMain.java
+++ JPA-BASIC/src/main/java/hellojpa/JpaMain.java
@@ -24,12 +24,19 @@ public class JpaMain {
             Member member = new Member();
             member.setUsername("member1");
             member.setTeam(team);
-            
             em.persist(member);
 
+
+            em.flush();
+            em.clear();
+
             Member findMember = em.find(Member.class, member.getId());
-            Team findTeam = findMember.getTeam();
-            System.out.println("findTeam.getName() = " + findTeam.getName());
+
+            List<Member> members = findMember.getTeam().getMembers();
+            for (Member m : members) {
+                System.out.println("m = " + m.getUsername());
+            }
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- JPA-BASIC/src/main/java/hellojpa/Team.java
+++ JPA-BASIC/src/main/java/hellojpa/Team.java
@@ -1,9 +1,8 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -14,6 +13,9 @@ public class Team {
     private Long id;
 
     private String name;
+
+    @OneToMany(mappedBy = "team") //Member 클래스에 있는 team //mappedBy 있는건 읽기만 가능. team이 연관관계 주인.
+    private List<Member> members = new ArrayList<>(); //초기화 해줌. 관례
 
     public Long getId() {
         return id;
@@ -29,5 +31,13 @@ public class Team {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
     }
 }
