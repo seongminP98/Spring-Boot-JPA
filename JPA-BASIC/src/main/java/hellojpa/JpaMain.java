517e310801d7f5737e95a8cab7fbc0971474b4a7
seongmin <psm7335@gmail.com>
2021-08-25 23:54:06 +0900

    단방향 연관관계

--- JPA-BASIC/src/main/java/hellojpa/JpaMain.java
+++ JPA-BASIC/src/main/java/hellojpa/JpaMain.java
@@ -16,86 +16,20 @@ public class JpaMain {
         tx.begin();
 
         try{
-/* 등록
-            Member member = new Member();
-            member.setId(2L);
-            member.setName("HelloB");
+            //저장
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
 
-            em.persist(member);
-*/
-/* 수정
-            Member findMember = em.find(Member.class, 1L); //멤버 찾기
-            findMember.setName("HelloJPA");
-*/
-/*
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
-
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-            }
-*/
-/*
-            //비영속
             Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
-            //영속
+            member.setUsername("member1");
+            member.setTeam(team);
+            
             em.persist(member);
 
-            Member findMember = em.find(Member.class, 101L);
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.name = " + findMember.getName()); //1차 캐시에서 가져옴.
-*/
-/*
-            Member findMember1 = em.find(Member.class, 101L); //DB에서 가져와 영속성 컨텍스트에 올려놓음
-            Member findMember2 = em.find(Member.class, 101L); //1차 캐시에서 가져옴.
-
-            System.out.println("result = " + (findMember1 == findMember2)); //영속 엔티티의 동일성 보장
-*/
-/*쓰기 지연. 커밋하고 DB에 쿼리 날림.
-            Member member1 = new Member(150L, "A");
-            Member member2 = new Member(160L, "B");
-
-            em.persist(member1);
-            em.persist(member2);
-            System.out.println("======================");
-*/
-/*
-            //데이터를 가져오고 값만 바꿔오면 수정됨. 변경감지(Dirty checking). 트랜잭션 커밋하는 시점에 업데이트 쿼리 날림.
-            Member member = em.find(Member.class, 150L);
-            member.setName("ZZZZZ");
-*/
-/*
-            Member member = new Member(202L, "member-202");
-            em.persist(member);
-
-            em.flush(); //플러시 강제로 호출. 이때 DB에 쿼리가 날라감. 1차캐시 유지됨.
-            System.out.println("===================");
-*/
-/*
-            Member member = em.find(Member.class, 150L);
-            //member는 영속상태.
-            member.setName("AAAA");
-
-            //em.detach(member); //member는 준영속상태가 됨. 트랜잭션 커밋해도 아무일도 일어나지 않음. AAAA로 변경안됨.
-            em.clear(); //영속성 컨텍스트를 완전히 초기화
-            Member member2 = em.find(Member.class, 150L); //다시 영속성컨텍스트를 올림.
-*/
-            Member member = new Member();
-            member.setUsername("C");
-
-            System.out.println("====================");
-            em.persist(member); //identity 전략을 사용하면 DB에 저장되어야 pk 값을 알 수 있기 때문에 커밋하기 전, 이 시점에 DB에 저장됨.
-            //영속성 컨텍스트는 pk 값이 있어야 하기 때문.
-
-            //SEQUENCE 전략을 사용할 때도 DB에 접근해서 pk값을 가져와야 함. 그래서 db에서 시퀀스 값을 얻어와서 member의 id값을 넣어줌.
-            //아직 db에 쿼리는 날라가지 않음. 시퀀스 값만 가져와서 member.id만 정해줌.
-            System.out.println("member.getId() = " + member.getId());
-            System.out.println("====================");
-
+            Member findMember = em.find(Member.class, member.getId());
+            Team findTeam = findMember.getTeam();
+            System.out.println("findTeam.getName() = " + findTeam.getName());
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- JPA-BASIC/src/main/java/hellojpa/Member.java
+++ JPA-BASIC/src/main/java/hellojpa/Member.java
@@ -1,42 +1,25 @@
 package hellojpa;
 
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
 
-
-/*
-//@SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
-@TableGenerator(
-        name = "MEMBER_SEQ_GENERATOR",
-        table = "MY_SEQUENCES",
-        pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
-*/
 
 @Entity
-@SequenceGenerator(
-        name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "MEMBER_SEQ", //매핑할 데이터베이스 시퀀스 이름
-        initialValue = 1, allocationSize = 50) //미리 시퀀스 값을 50개 땡겨옴. 기본값이 50
-public class Member {
 
-/*
-    //@GeneratedValue(strategy = GenerationType.IDENTITY) //mysql 에서 AUTO_ INCREMENT
-    //@GeneratedValue(strategy = GenerationType.SEQUENCE , generator = "member_seq_generator") //오라클, PostgreSQL, DB2, H2 에서 사용
-    @GeneratedValue(strategy = GenerationType.TABLE,
-            generator = "MEMBER_SEQ_GENERATOR")
-*/
+public class Member {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,
-            generator = "MEMBER_SEQ_GENERATOR")
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
 
-    @Column(name = "name", nullable = false) //DB의 컬럼명은 name, not null 제약조건
+    @Column(name = "USERNAME")
     private String username;
 
-    public Member(){
-    }
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID") //조인하는 컬럼. FK 가 TEAM_ID. 연관관계 매핑.
+    private Team team;
 
     public Long getId() {
         return id;
@@ -53,28 +36,13 @@ public class Member {
     public void setUsername(String username) {
         this.username = username;
     }
-    /* 필드와 컬럼매핑
-    private Integer age;
 
-    @Enumerated(EnumType.STRING) //기본이 EnumType.ORDINAL : enum 순서를 DB에 저장. (사용x)/ STRING은 enum 이름을 DB에 저장.
-    private RoleType roleType;
+    public Team getTeam() {
+        return team;
+    }
 
-    @Temporal(TemporalType.TIMESTAMP) //과거버전 timestamp, date는 TemporalType.date, time은 TemporalType.time
-    private Date createdDate;
-
-    @Temporal(TemporalType.TIMESTAMP) //과거버전 timestamp
-    private Date lastModifiedDate;
-
-    private LocalDate testLocalDate; //최신버전 date
-    private LocalDateTime testLocalDateTime; //최신버전 timestamp
-
-    @Lob //DB에 varchar를 넘어서는 큰 컨텐츠를 넣고싶으면 @Lob을 사용.
-    private String description;
-
-    @Transient //컬럼에 매핑하지 않음. 메모리에서만 사용. DB에 없음
-    private int temp;
-*/
-
-
+    public void setTeam(Team team) {
+        this.team = team;
+    }
 }
 
