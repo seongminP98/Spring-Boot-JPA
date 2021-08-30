cca86d0a16ffc3aae83306b49fd988c8472bab34
seongmin <psm7335@gmail.com>
2021-08-30 16:24:28 +0900

    값 타입과 불변 객체

--- JPA-BASIC/src/main/java/hellojpa/JpaMain.java
+++ JPA-BASIC/src/main/java/hellojpa/JpaMain.java
@@ -17,12 +17,32 @@ public class JpaMain {
         tx.begin();
 
         try{
+            /**
+             * member1, member2 둘 다 같은 address 값 타입을 사용하면.
+             * 난 member1의 city만 바꿀려고 했는데 member2의 city까지 바뀜.
+             * side effect
+             * 대신 값을 복사해서 사용해야 함.
+             */
+            Address address = new Address("city", "street", "10000");
 
             Member member = new Member();
-            member.setUsername("hello");
-            member.setHomeAddress(new Address("city","street","zipcode"));
-            member.setWorkPeriod(new Period());
+            member.setUsername("member1");
+            member.setHomeAddress(address);
             em.persist(member);
+
+            Address newAddress = new Address("NewCity", address.getStreet(), address.getZipcode());
+            member.setHomeAddress(newAddress); //값을 바꿀려면 다시 만들어서 통으로 바꿈.
+
+
+            Address copyAddress  = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+
+            Member member2 = new Member();
+            member2.setUsername("member2");
+            member2.setHomeAddress(copyAddress);
+            em.persist(member2);
+
+//            member.getHomeAddress().setCity("newCity");
+//세터지움. 아니면 private으로 세터를 만들어둠.
 
 
             tx.commit();
