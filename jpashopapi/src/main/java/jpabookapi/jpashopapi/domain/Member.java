0ebf56850e9533ed34f28e0d03a8043cc7c89318
seongmin <psm7335@naver.com>
2021-09-14 20:16:20 +0900

    회원 등록 API

--- jpashopapi/src/main/java/jpabookapi/jpashopapi/domain/Member.java
+++ jpashopapi/src/main/java/jpabookapi/jpashopapi/domain/Member.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,6 +16,7 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty
     private String name;
 
     @Embedded //(Embedded, Embeddable 하나만 있어도 됨. 여기선 둘 다 써줬음.)
