eb1dbbffb4ab2088210ac1cd036090c32e3af890
seongmin <psm7335@naver.com>
2021-09-16 22:54:57 +0900

    주문 조회 V5: JPA에서 DTO 직접 조회 - 컬렉션 조회 최적화

--- jpashopapi/src/main/java/jpabookapi/jpashopapi/api/OrderApiController.java
+++ jpashopapi/src/main/java/jpabookapi/jpashopapi/api/OrderApiController.java
@@ -77,6 +77,11 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> ordersV5() {
+        return orderQueryRepository.findAllDto_optimization();
+    }
+
     @Getter
     static class OrderDto {
 
