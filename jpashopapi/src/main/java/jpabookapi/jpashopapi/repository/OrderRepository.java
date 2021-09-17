bd5c90007bfba72760a774c02446f19a5f156a75
seongmin <psm7335@naver.com>
2021-09-17 16:48:12 +0900

    QueryDSL 소개

--- jpashopapi/src/main/java/jpabookapi/jpashopapi/repository/OrderRepository.java
+++ jpashopapi/src/main/java/jpabookapi/jpashopapi/repository/OrderRepository.java
@@ -1,6 +1,8 @@
 package jpabookapi.jpashopapi.repository;
 
-import jpabookapi.jpashopapi.domain.Member;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jpabookapi.jpashopapi.domain.*;
 import jpabookapi.jpashopapi.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -92,6 +94,34 @@ public class OrderRepository {
         cq.where(cb.and(criteria.toArray(new Predicate[criteria.size()])));
         TypedQuery<Order> query = em.createQuery(cq).setMaxResults(1000); //최대1000건
         return query.getResultList();
+    }
+
+    public List<Order> findAllQueryDsl(OrderSearch orderSearch) {
+        JPAQueryFactory query = new JPAQueryFactory(em);
+        QOrder order = QOrder.order;
+        QMember member = QMember.member;
+
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()), nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression nameLike(String memberName) {
+        if (!StringUtils.hasText(memberName)) {
+            return null;
+        }
+        return QMember.member.name.like(memberName);
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+        if (statusCond == null) {
+            return null;
+        }
+        return QOrder.order.status.eq(statusCond);
     }
 
     public List<Order> findAllWithMemberDelivery() {
