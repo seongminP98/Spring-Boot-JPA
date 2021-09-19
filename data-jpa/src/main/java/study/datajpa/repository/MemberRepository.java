7713f48fdbbeeb12284a727b2f3207438d73c939
seongmin <psm7335@naver.com>
2021-09-19 14:17:55 +0900

    메소드 이름으로 쿼리 생성

--- data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
+++ data-jpa/src/main/java/study/datajpa/repository/MemberRepository.java
@@ -8,4 +8,10 @@ import java.util.List;
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     List<Member> findByUsername(String username); //구현하지 않아도 동작함. 쿼리메소드 기능
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    List<Member> findHelloBy(); //전체 조회.  find...By ...엔 아무거나 들어가도 됨.
+
+    List<Member> findTop3HelloBy(); //limit 3개
 }
