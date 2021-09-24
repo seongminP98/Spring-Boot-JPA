7c8cbd933615dd440a252dd7a3c95f2f2e606586
seongmin <psm7335@naver.com>
2021-09-25 01:35:16 +0900

    새로운 엔티티를 구별하는 방법

---
+++ data-jpa/src/main/java/study/datajpa/entity/Item.java
@@ -0,0 +1,42 @@
+package study.datajpa.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Item implements Persistable<String> {
+    /**
+     * JPA 식별자 생성 전략이 @GeneratedValue가 아니라 직접 할당이면 implements Persistable 사용해서 isNew 구현.
+     */
+    @Id
+    private String id;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    public Item(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getId() {
+        return null;
+    }
+
+    @Override
+    public boolean isNew() {
+        return createdDate == null;
+    }
+}
