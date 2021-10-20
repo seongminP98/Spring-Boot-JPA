0aa78d877b8a844ce431866f5eca1f314df5f2fa
seongmin <psm7335@naver.com>
2021-10-20 17:01:33 +0900

    프로젝션과 결과 반환 - @QueryProjection

--- querydsl/src/main/java/study/querydsl/dto/MemberDto.java
+++ querydsl/src/main/java/study/querydsl/dto/MemberDto.java
@@ -1,5 +1,6 @@
 package study.querydsl.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,7 +11,7 @@ public class MemberDto {
     private String username;
     private int age;
 
-
+    @QueryProjection //Q파일 생성. gradle 에서 compileQuerydsl
     public MemberDto(String username, int age) {
         this.username = username;
         this.age = age;
